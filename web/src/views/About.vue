cdaff7e9c65b874fbd3eef300a039d6798132b63
Mohammad Rahhal <m.r992@hotmail.com>
2019-02-23 20:57:37 +0200

    Update About.vue

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    About me!
+  <div class="about-page">
+    <div>
+      Mohammad Rahhal @mrahhal
+    </div>
+
+    <div>
+      <a href="github.com/mrahhal" target="_blank">GitHub</a>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.about-page {
+  text-align: center;
+}
+</style>

c121a55e3f3f643c0937f1d36f90ba7cef702620
Mohammad Rahhal <m.r992@hotmail.com>
2019-02-23 15:33:32 +0200

    Setup relative and implicit imports of scss

---
+++ web/vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        includePaths: [path.resolve(__dirname, './src/scss')],
+        data: `
+          @import 'mixins';
+          @import 'variables';
+        `
+      }
+    }
+  }
+};

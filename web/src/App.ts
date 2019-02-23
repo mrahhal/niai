879dfae83f4d5b498a066699d83a891cb4a1f60c
Mohammad Rahhal <m.r992@hotmail.com>
2019-02-23 22:31:57 +0200

    Correctly setup component subscriptions for theme

--- web/src/App.ts
+++ web/src/App.ts
@@ -1,12 +1,23 @@
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { getTheme, themeChanges } from './services/theme';
+import { Component, Vue } from 'vue-property-decorator';
+
+import { getTheme, setTheme, themeChanges } from './services/theme';
 
 @Component
 export default class App extends Vue {
   private theme = getTheme();
 
-  constructor() {
-    super();
-    themeChanges.subscribe(theme => this.theme = theme);
+  created() {
+    themeChanges.subscribe(theme => {
+      this.theme = theme;
+    });
+  }
+
+  update() {
+    this.theme = this.theme;
+  }
+
+  toggleTheme() {
+    const newTheme = getTheme() === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
   }
 }

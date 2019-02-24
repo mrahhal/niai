e001b01276a42ee821e8228799fbdfb8617fc5ad
Mohammad Rahhal <m.r992@hotmail.com>
2019-02-24 17:49:43 +0200

    Update some styles

--- web/src/components/kanji-card/kanji-card.vue
+++ web/src/components/kanji-card/kanji-card.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="kanji-card">
-    <div class="kanji-card-character">{{kanji.character}}</div>
-    <div class="kanji-card-meaning">{{kanji.meanings.join(', ')}}</div>
-    <div class="kanji-card-reading">
-      {{kanji.onyomi}}
-      <br>
-      {{kanji.kunyomi}}
+    <div class="kanji-card-top">
+      <div class="kanji-card-character">{{kanji.character}}</div>
+      <div class="label">{{kanji.frequency}}</div>
+    </div>
+    <div class="kanji-card-meaning mt-1">{{kanji.meanings.join(', ')}}</div>
+    <div class="kanji-card-reading mt-1">
+      <div class="kanji-card-reading-on">{{kanji.onyomi}}</div>
+      <div class="kanji-card-reading-kun">{{kanji.kunyomi}}</div>
+    </div>
+    <div class="label-list mt-1">
+      <div class="label" v-for="tag in kanji.tags" :title="tag.value" :key="tag.key">{{tag.key}}</div>
     </div>
   </div>
 </template>

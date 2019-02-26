59d595fe9d3366599b2555c048ee3e570a630157
Mohammad Rahhal <m.r992@hotmail.com>
2019-02-26 19:25:51 +0200

    Add common class to svg icons

--- web/src/components/svgs/svg-spinner.vue
+++ web/src/components/svgs/svg-spinner.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="svg-spinner">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="niai-icon svg-spinner">
     <circle
       cx="50"
       cy="50"

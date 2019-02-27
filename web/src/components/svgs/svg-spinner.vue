199cb9fdec863d59b0091d7913cc8bfa937b63b9
Mohammad Rahhal <m.r992@hotmail.com>
2019-02-27 15:15:52 +0200

    Replace svg-spinner

--- web/src/components/svgs/svg-spinner.vue
+++ web/src/components/svgs/svg-spinner.vue
@@ -1,26 +1,54 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="niai-icon svg-spinner">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66 66" class="niai-icon svg-spinner">
     <circle
-      cx="50"
-      cy="50"
+      class="path"
       fill="none"
+      stroke-width="6"
       stroke-linecap="round"
-      r="40"
-      stroke-width="4"
       stroke="currentColor"
-      stroke-dasharray="62.83185307179586 62.83185307179586"
-      transform="rotate(276.474 50 50)"
-    >
-      <animateTransform
-        attributeName="transform"
-        type="rotate"
-        calcMode="linear"
-        values="0 50 50;360 50 50"
-        keyTimes="0;1"
-        dur="1s"
-        begin="0s"
-        repeatCount="indefinite"
-      ></animateTransform>
-    </circle>
+      cx="33"
+      cy="33"
+      r="30"
+    ></circle>
   </svg>
 </template>
+
+<style lang="scss" scoped>
+$offset: 187;
+$duration: 1.4s;
+
+.svg-spinner {
+  animation: rotator $duration linear infinite;
+
+  .path {
+    stroke-dasharray: $offset;
+    stroke-dashoffset: 0;
+    transform-origin: center;
+    animation: dash $duration ease-in-out infinite;
+  }
+}
+
+@keyframes rotator {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(270deg);
+  }
+}
+
+@keyframes dash {
+  0% {
+    stroke-dashoffset: $offset;
+  }
+  50% {
+    stroke-dashoffset: $offset/4;
+    transform: rotate(135deg);
+  }
+  100% {
+    stroke-dashoffset: $offset;
+    transform: rotate(450deg);
+  }
+}
+</style>
+

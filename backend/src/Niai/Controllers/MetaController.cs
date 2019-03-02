ae7c317b557ea823a9e8add9ab386acf7a34efe3
Mohammad Rahhal <m.r992@hotmail.com>
2019-03-02 14:23:09 +0200

    Create MetaController.cs

---
+++ backend/src/Niai/Controllers/MetaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Niai.Models;
+using Niai.Services;
+
+namespace Niai.Controllers
+{
+	public class MetaController : NiaiControllerBase
+	{
+		private readonly IDataProvider _dataProvider;
+
+		public MetaController(
+			IDataProvider dataProvider)
+		{
+			_dataProvider = dataProvider;
+		}
+
+		[HttpGet]
+		public ActionResult<Metadata> Get()
+		{
+			return Ok(_dataProvider.Metadata);
+		}
+	}
+}

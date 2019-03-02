b0b366b0b01953cb94d7edbf115ad0870a08941c
Mohammad Rahhal <m.r992@hotmail.com>
2019-03-02 18:39:49 +0200

    Update about page
    Closes #7

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,4 +1,5 @@
 import { SearchResult } from '@/models';
+import { Metadata } from '@/models/metadata';
 import axios from 'axios';
 
 const API_URL = process.env.VUE_APP_API_URL;
@@ -7,6 +8,11 @@ export class Api {
   async search(q: string) {
     const params = new URLSearchParams({ q });
     const response = await axios.get<SearchResult>(`${API_URL}/api/search`, { params });
+    return response.data;
+  }
+
+  async getMetadata() {
+    const response = await axios.get<Metadata>(`${API_URL}/api/meta`);
     return response.data;
   }
 }

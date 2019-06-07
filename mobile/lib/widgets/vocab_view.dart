c95bebca3b057c3e0f3d4435c8817c0b1391f886
Mohammad Rahhal <m.r992@hotmail.com>
2019-06-07 17:11:48 +0300

    Update vocab_view.dart

--- mobile/lib/widgets/vocab_view.dart
+++ mobile/lib/widgets/vocab_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:niai/models/models.dart';
 
 class VocabView extends StatelessWidget {
@@ -11,14 +12,30 @@ class VocabView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Theme.of(context).cardColor,
-        border: Border.all(width: 1),
-        borderRadius: BorderRadius.all(Radius.circular(2)),
+    return InkWell(
+      onTap: () {
+        Clipboard.setData(new ClipboardData(text: vocab.kanji));
+
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text('Copied "${vocab.kanji}"'),
+        ));
+      },
+      child: Container(
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          border: Border(
+              bottom:
+                  BorderSide(width: 1, color: Theme.of(context).dividerColor)),
+        ),
+        child: Row(
+          children: <Widget>[
+            Text(
+              vocab.kanji,
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
       ),
-      child: Text(vocab.kanji),
     );
   }
 }

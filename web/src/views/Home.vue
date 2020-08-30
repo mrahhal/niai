bc2f6fde96a8d9a5afdea2a2608e7781b0854492
Mohammad Rahhal <m.r992@hotmail.com>
2020-08-30 13:31:57 +0300

    Add donation button

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -84,6 +84,19 @@
 
       <hr>
 
+      <div class="mb-3">
+        <strong>Donations help keep the server running, improve this app and work on new ones for Japanese, all while keeping everything ad-free. Thank you!</strong>
+      </div>
+
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick" />
+        <input type="hidden" name="hosted_button_id" value="Z33348GZ36RSU" />
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+      </form>
+
+      <hr>
+
       <recent-changes></recent-changes>
     </div>
   </div>

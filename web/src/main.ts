78632ba7139c152fdacfebfe9f230aaee48c486f
Mohammad Rahhal <m.r992@hotmail.com>
2023-02-28 11:08:29 +0100

    Update to vue-gtag

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,17 +1,16 @@
 import '@/components/globals';
 
 import Vue from 'vue';
-import VueAnalytics from 'vue-analytics';
 
+import VueGtag from 'vue-gtag';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
 
 if (process.env.NODE_ENV === 'production') {
-  Vue.use(VueAnalytics, {
-    id: process.env.VUE_APP_GOOGLE_ANALYTICS_TRACKING_ID,
-    router,
+  Vue.use(VueGtag, {
+    config: { id: process.env.VUE_APP_GOOGLE_ANALYTICS_TRACKING_ID },
   });
 }
 
--- web/src/typings/vue-analytics.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'vue-analytics' {
-  var A: any;
-  export default A;
-}
